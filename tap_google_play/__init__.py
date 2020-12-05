c536e0c6d9ac54e44ffad173ea550c292dacfffe
David Lequin <david.lequin@gmail.com>
2020-12-05 14:28:31 +0000

    typing error

--- tap_google_play/__init__.py
+++ tap_google_play/__init__.py
@@ -76,8 +76,8 @@ def scroll(host, collection, category, country_code):
 def app_list(config):
     category_l = ['APPLICATION','ANDROID_WEAR','ART_AND_DESIGN','AUTO_AND_VEHICLES','BEAUTY','BOOKS_AND_REFERENCE','BUSINESS','COMICS','COMMUNICATION','DATING','EDUCATION','ENTERTAINMENT','EVENTS','FINANCE','FOOD_AND_DRINK','HEALTH_AND_FITNESS','HOUSE_AND_HOME','LIBRARIES_AND_DEMO','LIFESTYLE','MAPS_AND_NAVIGATION','MEDICAL','MUSIC_AND_AUDIO','NEWS_AND_MAGAZINES','PARENTING','PERSONALIZATION','PHOTOGRAPHY','PRODUCTIVITY','SHOPPING','SOCIAL','SPORTS','TOOLS','TRAVEL_AND_LOCAL','VIDEO_PLAYERS','WEATHER','GAME','GAME_ACTION','GAME_ADVENTURE','GAME_ARCADE','GAME_BOARD','GAME_CARD','GAME_CASINO','GAME_CASUAL','GAME_EDUCATIONAL','GAME_MUSIC','GAME_PUZZLE','GAME_RACING','GAME_ROLE_PLAYING','GAME_SIMULATION','GAME_SPORTS','GAME_STRATEGY','GAME_TRIVIA','GAME_WORD','FAMILY','FAMILY_ACTION','FAMILY_BRAINGAMES','FAMILY_CREATE','FAMILY_EDUCATION','FAMILY_MUSICVIDEO','FAMILY_PRETEND']
     collection_l = ['topselling_free','topselling_paid','topgrossing','movers_shakers','topselling_free_games','topselling_paid_games','topselling_grossing_games','topselling_new_free','topselling_new_paid','topselling_new_free_games','topselling_new_paid_games']
-    n_category = config['categories_to_scrape']
-    n_collection = config['collections_to_scrape']
+    n_category = int(config['categories_to_scrape'])
+    n_collection = int(config['collections_to_scrape'])
     full_app_list = []
     for country_code in config['country_code']:
         for category in category_l[:n_category]:
